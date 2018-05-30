0e8b44e2787a976621544c46db53f1009e29a0b4
David Gu <macdavid313@gmail.com>
2018-05-30 21:07:58 +0800

    update 'ensure-double-float'

--- src/sys/utils.lisp
+++ src/sys/utils.lisp
@@ -3,9 +3,14 @@
 
 (defmacro ensure-double-float (vars &body body)
   "Take a list of varibles, ensure each to be of type double-float and throw an error if one is not of type real."
-  `(let ,(mapcar (lambda (var)
-                   `(,var (etypecase ,var
-                            (double-float ,var)
-                            (real (coerce ,var 'double-float)))))
-                 vars)
-     ,@body))
+  (alexandria:with-gensyms (r i)
+    `(let ,(mapcar (lambda (var)
+                     `(,var (etypecase ,var
+                              (double-float ,var)
+                              (real (coerce ,var 'double-float))
+                              (complex (let ((,r (realpart ,var))
+                                             (,i (imagpart ,var)))
+                                         (complex (coerce ,r 'double-float)
+                                                  (coerce ,i 'double-float)))))))
+                   vars)
+       ,@body)))
